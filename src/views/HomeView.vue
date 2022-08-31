2b5ac809d2d1889adddb42cb897eb4cc1c2083de
msplint <tk@terrifickid.net>
2022-08-31 18:20:05 -0500

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,29 @@
 <template>
-  <div
-    class="flex min-h-screen min-w-full bg-black text-white items-center justify-end"
-  >
-    <div class="absolute top-0 left-0 p-6">
-      <p class="text-xl font-mono">uz1</p>
-      <p class="text-lg font-ak">systems architecture and blockchain</p>
-      <p class="text mt-2 pt-2">contact@uz1.co.uk</p>
+  <div class="min-h-screen min-w-full bg-gg text-white grid grid-cols-12">
+    <div class="absolute bottom-3 left-3 right-3 text-center md:text-left">
+      <p class="text-xl text-white font-mono uppercase">
+        blockchain * web * creative
+      </p>
     </div>
-    <div class="absolute bottom-0 right-0 p-6"></div>
+    <div class="absolute top-3 left-0 right-0 text-center"></div>
+    <div
+      class="absolute w-screen h-screen justify-center flex items-center z-50"
+    >
+      <a
+        class="hover:underline text-2xl text-yy font-mono uppercase"
+        href="mailto:contact@uz1.co.uk"
+      >
+        contact<span style="font-family: sans-serif">@</span>uz1.co.uk
+      </a>
+    </div>
+
+    <div class="col-span-12 md:col-span-6 p-3 text-center md:text-left">
+      <span class="font-mono text-white text-6xl">UZ<span>1</span></span>
+    </div>
+    <div
+      class="col-span-6 bg-cover"
+      style="background-image: url('8.svg')"
+    ></div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        gg: "#1d1d1b",
+        yy: "#ffff00",
+      },
       fontFamily: {
         mono: ["AkkuratMonoLLWebTrial-Regular"],
         ak: ["AkkuratLLWebTrial-Light"],
